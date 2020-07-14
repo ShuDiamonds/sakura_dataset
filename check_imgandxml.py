36a2a8ab9bb380b9c35af923bf7e26b8cc5f2ed0
fukudashuichi <fukudaubuntu@gmail.com>
2020-07-14 09:16:29 +0900

    xmlはあるけど、アノテーションがされてない場合を検出できるようにした

--- check_imgandxml.py
+++ check_imgandxml.py
@@ -6,28 +6,19 @@ import re
 from PIL import Image
 
 if __name__ == '__main__':
-    xmlfolderpath = "./../FishDB_annotations_png_xml/"
-    #xmlfolderpath = "./../valid_xml/"
+    xmlfolderpath = "./annotations/"
 
-    #xmlfolderpath = "./../koura/xml/"
-    #xmlfolderpath = "./../UnderwaterPhotography/xml/"
-    imgfilepath = "./../FishDB_images_png/"
-    #imgfilepath = "./../valid_img/"
-    #imgfilepath = "./../koura/png/"
-    #imgfilepath = "./../UnderwaterPhotography/nagisa_park/"
+    imgfilepath = "./images/"
 
-    imgfilepath = "./../smallfish/"
-    xmlfolderpath = "./../smallfish_xml/"
 
-    #outputxmlpath=xmlfolderpath+'output/'
-    outputxmlpath=xmlfolderpath
-    os.makedirs(outputxmlpath, exist_ok=True)
+    outputxmlpath=xmlfolderpath+'output/'
+    #os.makedirs(outputxmlpath, exist_ok=True)
 
     xmlfiles = [ x.split(".")[0] for x in sorted(os.listdir(xmlfolderpath)) if x!=".DS_Store" and x!="output"]
     imgfiles = [ x.split(".")[0] for x in sorted(os.listdir(imgfilepath)) if x!=".DS_Store"]
     imgfiles_exts=[ x.split(".")[-1] for x in sorted(os.listdir(imgfilepath)) if x!=".DS_Store"]
 
-    print(xmlfiles)
+    #print(xmlfiles)
     #print(imgfiles)
     print("##################### check xml files")
     for i,xmlfilename in enumerate(xmlfiles):
@@ -39,6 +30,9 @@ if __name__ == '__main__':
         #print(xmlfilename)
         #print(rawfile)
         #print(re.findall("<width>\d{1,4}</width>",rawfile))
+        print(re.findall("<xmax>\d{1,4}</xmax>",rawfile))
+        if len(re.findall("<xmax>\d{1,4}</xmax>",rawfile))==0:
+            print("ERROR: xml file exists, but No anottation on ",xmlfilename)
         width=int(re.sub(r"\D", "", re.findall("<width>\d{1,4}</width>",rawfile)[0])  )
         height=int( re.sub(r"\D", "", re.findall("<height>\d{1,4}</height>",rawfile)[0]) )
         xmax=int(re.sub(r"\D", "", re.findall("<xmax>\d{1,4}</xmax>",rawfile)[0])  )
@@ -48,15 +42,12 @@ if __name__ == '__main__':
         if height<ymax:
             print("Error annotation: height<ymax:")
         targetfilename=re.findall("<filename>.*</filename>",rawfile)[0]
-        #print(targetfilename)
-        #print(rawfile)
+
         ######### filename の変更
         rawfile=re.sub(r".png</filename>",r".jpg</filename>",rawfile)
-        #print(rawfile)
         ######### <path>の削除
         rawfile=re.sub(r"<path>.*</path>","",rawfile)
         im = Image.open(imgfilepath+xmlfilename+"."+imgfiles_exts[i])
-        #print(im.size)
         img_width=int(im.size[0])
         img_height=int(im.size[1])
 
@@ -66,8 +57,8 @@ if __name__ == '__main__':
             print("different height ({0},{1}): {2}".format(height,img_height,imgfilepath+xmlfilename) )
 
         # ファイルの書き込み
-        with open(outputxmlpath+xmlfilename+".xml",mode='w') as f:
-            f.write(rawfile)
+        # with open(outputxmlpath+xmlfilename+".xml",mode='w') as f:
+        #     f.write(rawfile)
 
 
     print("##################### check img files")
@@ -75,9 +66,9 @@ if __name__ == '__main__':
         if not imgfilename in xmlfiles:
             print("Error: No annotation file. " + imgfilename + "."+imgfiles_exts[i]+  " will be deleted, [y/n] ")
             ans=input()
-            if ans=="y":
-                print(imgfilepath+imgfilename+"."+sorted(os.listdir(imgfilepath))[i].split(".")[1]+" is deleted.")
-                os.remove( imgfilepath+imgfilename+"."+sorted(os.listdir(imgfilepath))[i].split(".")[1] )
+            if ans!="n":
+                print(imgfilepath+imgfilename+"."+imgfiles_exts[i]+" is deleted.")
+                os.remove( imgfilepath+imgfilename+"."+imgfiles_exts[i] )
 
             else:
-                print(imgfilepath + imgfilename+"."+sorted(os.listdir(imgfilepath))[i].split(".")[1] + " is not deleted.")
+                print(imgfilepath + imgfilename+"."+imgfiles_exts[i] + " is not deleted.")
